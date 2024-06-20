eacaa4794d9fb043556fae798a6b408a05bae72c
Kali Chaudhary <kalichaudhary@gmail.com>
2024-06-20 17:01:06 +0545

    Add: hover image for about section

--- wp-content/themes/hiuptwentyfour/App/CustomFields/AboutUsFlexibleFields.php
+++ wp-content/themes/hiuptwentyfour/App/CustomFields/AboutUsFlexibleFields.php
@@ -30,12 +30,12 @@ class AboutUsFlexibleFields
                     ->set_collapsed(true)
 
                     //About Content Layout
-                    ->add_fields('about_us_wysiwyg_layout', 'Wysiwyg Editor Layout', [
+                    ->add_fields('about_us_wysiwyg_layout', 'Wysiwyg Editor - Layout', [
                         Field::make('rich_text', 'about_us_wysiwyg', 'WYSIWYG Editor'),
                     ])
 
                     //About Image Layout
-                    ->add_fields('about_us_content_layout', 'Image(2)/Column(3) Image Layout', [
+                    ->add_fields('about_us_content_layout', 'Image(2)/Column(3) Image - Layout', [
                         Field::make('complex', 'left', 'Left')
                             ->set_duplicate_groups_allowed(false)
                             ->set_width(33.33)
@@ -60,7 +60,7 @@ class AboutUsFlexibleFields
                     ])
 
                     //Steps Layout
-                    ->add_fields('steps_layout', 'Steps Layout', [
+                    ->add_fields('steps_layout', 'Steps - Layout', [
                         Field::make('text', 'crb_title', __('Title'))->set_attribute('placeholder', 'Title')->set_width(33.33),
                         Field::make('text', 'crb_button_contact_us_url', __('Button URL (Contact Us)'))->set_attribute('placeholder', 'https://example.com/contact-us')->set_width(33.33),
                         Field::make('separator', 'crb_separator_about_us', __('3 Steps Section')),
@@ -91,8 +91,8 @@ class AboutUsFlexibleFields
                             ]),
                     ])
 
-                    //Steps Layout
-                    ->add_fields('about_us_wysiwyg_image_counter_layout', 'Wysiwyg Editor - Image - Counter Layout', [
+                    //Counter Layout
+                    ->add_fields('about_us_wysiwyg_image_counter_layout', 'Wysiwyg Editor, Image, Counter - Layout', [
                         Field::make('text', 'crb_wysiwyg_image_counter_layout_title', __('Title'))->set_attribute('placeholder', 'Title'),
                         Field::make('textarea', 'crb_wysiwyg_image_counter_layout_short_description', __('Short Description'))->set_attribute('placeholder', 'Short Description - Max 50 words'),
 
@@ -102,7 +102,7 @@ class AboutUsFlexibleFields
                             ->add_fields([
                                 Field::make('image', 'image_block', 'Image'),
                             ]),
-                        Field::make('complex', 'right', 'Description and Counter')
+                        Field::make('complex', 'right', 'Description and Counter - Layout')
                             ->set_duplicate_groups_allowed(false)
                             ->set_width(80)
                             ->add_fields([
@@ -138,6 +138,91 @@ class AboutUsFlexibleFields
                                     ]),
                             ]),
                     ])
+
+
+                    //Vision Layout
+                    ->add_fields('about_us_wysiwyg_image_layout', 'Wysiwyg Editor Image - Layout', [
+                        Field::make('complex', 'left', 'Title and Description')
+                            ->set_duplicate_groups_allowed(false)
+                            ->set_width(60)
+                            ->add_fields([
+                                Field::make('text', 'crb_wysiwyg_image_counter_layout_title', __('Title'))->set_attribute('placeholder', 'Title'),
+                                Field::make('textarea', 'crb_wysiwyg_image_counter_layout_short_description', __('Short Description'))->set_attribute('placeholder', 'Short Description - Max 60 words'),
+                                Field::make('text', 'crb_wysiwyg_image_counter_layout_title', __('Title'))->set_attribute('placeholder', 'Title'),
+                                Field::make('textarea', 'crb_wysiwyg_image_counter_layout_short_description', __('Short Description'))->set_attribute('placeholder', 'Short Description - Max 60 words'),
+                                Field::make('text', 'crb_wysiwyg_image_counter_layout_title', __('Title'))->set_attribute('placeholder', 'Title'),
+                                Field::make('textarea', 'crb_wysiwyg_image_counter_layout_short_description', __('Short Description'))->set_attribute('placeholder', 'Short Description - Max 60 words'),
+                            ]),
+                        Field::make('complex', 'right', 'Image')
+                            ->set_duplicate_groups_allowed(false)
+                            ->set_width(40)
+                            ->add_fields([
+                                Field::make('image', 'image_block', 'Image'),
+                            ]),
+                    ])
+
+                    // Brand Layout
+                    ->add_fields('about_us_brand_layout', 'Brand Logos - Layout', [
+                        Field::make('complex', 'brand_logos', 'Brand Logos')
+                            ->set_width(10)
+                            ->add_fields([
+                                Field::make('image', 'image_brand', 'Brand Logo'),
+                            ]),
+                    ])
+
+
+                    //Award and Team Layout
+                    ->add_fields('about_us_award_team_layout', 'Award and Team - Layout', [
+                        Field::make('complex', 'left', 'Awarded Team')
+                            ->set_duplicate_groups_allowed(false)
+                            ->set_width(33.33)
+                            ->add_fields([
+                                Field::make('image', 'image_block', 'Image'),
+                                Field::make('text', 'crb_award_team_title', __('Title'))->set_attribute('placeholder', 'Awards`N Stuff -Title'),
+                                Field::make('textarea', 'crb_award_team_card_award_description', __('Award Description'))->set_attribute('placeholder', 'Award Description - Max 3 words'),
+                                Field::make('text', 'crb_award_team_card_award_year', __('Award Year'))->set_attribute('placeholder', 'Award Year'),
+                            ]),
+                        Field::make('complex', 'middle', 'Meat Our Team')
+                            ->set_width(33.33)
+                            ->add_fields([
+                                Field::make('image', 'meat_our_team_image', 'Team Avatar'),
+                                Field::make('text', 'crb_avatar_name', __('Name'))->set_attribute('placeholder', 'Avatar Name'),
+                                Field::make('text', 'crb_avatar_designation', __('Designation'))->set_attribute('placeholder', 'Avatar Designation')->set_width(44.44),
+                                Field::make('text', 'crb_avatar_rating', __('Rating'))->set_attribute('placeholder', 'Avatar Rating')->set_width(22.22),
+                            ]),
+                        Field::make('complex', 'right', 'Meat Our Team')
+                            ->set_duplicate_groups_allowed(false)
+                            ->set_width(33.33)
+                            ->add_fields([
+                                Field::make('text', 'crb_join_team_url', __('Join Team URL'))->set_attribute('placeholder', 'Join our team'),
+                            ]),
+                    ])
+
+                    //Action box Layout
+                    ->add_fields('about_us_action_box_layout', 'Action Box - Layout', [
+                        Field::make('complex', 'left', 'Left - Action Avatar')
+                            ->set_duplicate_groups_allowed(false)
+                            ->set_width(33.33)
+                            ->add_fields([
+                                Field::make('image', 'action_avatar_image', 'Action Avatar'),
+                                Field::make('text', 'crb_action_box_avatar_name', __('Name'))->set_attribute('placeholder', 'Avatar Name'),
+                                Field::make('text', 'crb_action_box_avatar_designation', __('Designation'))->set_attribute('placeholder', 'Avatar Designation'),
+                            ]),
+                        Field::make('complex', 'middle', 'Middle - Wysiwyg Description')
+                            ->set_duplicate_groups_allowed(false)
+                            ->set_width(33.33)
+                            ->add_fields([
+                                Field::make('textarea', 'crb_action_box_wysiwyg_description', __('Wysiwyg Description'))->set_attribute('placeholder', 'Wysiwyg Description - Max 50 words'),
+                            ]),
+                        Field::make('complex', 'right', 'Right - Action Decoration Image')
+                            ->set_duplicate_groups_allowed(false)
+                            ->set_width(33.33)
+                            ->add_fields([
+                                Field::make('image', 'action_decoration_image', 'Action Decoration Image (PNG format)'),
+                            ]),
+                    ])
+
+
             ]);
     }
 }
